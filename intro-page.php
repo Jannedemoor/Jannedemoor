e0dbff1ad89bd6ac92988dfa2ce194eeba37eec3
Jannedemoor <63249983+Jannedemoor@users.noreply.github.com>
2020-04-06 17:34:55 +0200

    Add files via upload

---
+++ intro-page.php
@@ -0,0 +1,27 @@
+f<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Intro </title>
+		<link rel="stylesheet" href="../layout/css/css-intro.css">
+	</head>
+	<body>
+	<header>
+		<div class="main">
+			<ul>
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="contact-page.php">Contact</a></li>
+			</ul>
+		</div>
+		<div class="title">
+			<h1>Liste des CV du groupe LPOW C1 D</h1>
+		</div>
+		<div class="button">
+			<a href="./cv/cv-janne.php" class="btn">CV JANNE </a>
+			<a href="./cv/cv-laury.php"class="btn">CV LAURY</a>
+			<a href="./cv/cv-anastasia.php"class="btn">CV ANASTASIA</a>
+			<a href="./cv/cv-sebastian.php"class="btn">CV SEBASTIAN</a>
+		</div>
+	</header>
+	
+</html>
